c8309e610f49fba777f0259c919c9ae839f75100
Harry <hfh@Harrys-MacBook-Air.local>
2021-07-29 17:05:21 +0100

    Intial commit - most of the frontend complete

---
+++ README.md
@@ -0,0 +1,6 @@
+# Full Crud API application using NodeJS, Express and MongoDB
+
+## Simple User Management System
+Built from scratch using html and css styling for the frontend.
+
+Ability to add, delete and edit users based on some basic parameters.
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+// useful for sharing code, keep secrets out of harms way
+dotenv.config( {path : "config.env "} )
+const PORT = process.env.PORT || 8080
+
+// Log Requests
+app.use(morgan('tiny'));
+
+// Parse requests to body-parser
+app.use(bodyparser.urlencoded( {extended:true} ))
+
+// Set View Engine
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname, "views/ejs")
+
+// Load Assets - using use method (specified the path) to specify the style.css use css/style.css
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+
+app.get('/',(req , res)=>{
+   res.render('index'); // method allows us to render a html file extension is already specified above!
+})
+
+app.listen(PORT,()=>{console.log(`Server is running on http://localhost:${PORT}`)});
