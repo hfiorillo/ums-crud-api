71a5250ef44fd8c98d3c2b4407d179391f06e4b4
Will Dewhurst <34936646+dewhurstwill@users.noreply.github.com>
2021-09-04 15:13:11 +0100

    Formatting

--- server/database/connection.js
+++ server/database/connection.js
@@ -13,10 +13,10 @@ const connectDB = async () => {
         })
 
         console.log(`MongoDB connected: ${con.connection.host}`);
-    }catch(err){
+    } catch(err) {
         console.log(err);
         process.exit(1);
     }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
